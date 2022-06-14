ccb7dbcbd50338dd093a41bfab73160814414eb9
Ivan <ivan.spiridonov.01@inbox.ru>
2022-06-14 23:22:01 +0300

    Modified index

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
 });
 
+app.get('/data', (req, res) => {
+    res.send({ message: 'this page has data' });
+});
+
 app.listen(8089, () => {
     console.log('Application listening!');
 });
