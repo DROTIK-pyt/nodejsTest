1b2e8bd11454a5dad30d0396e19a7e416102f8ee
Ivan <ivan.spiridonov.01@inbox.ru>
2022-06-16 10:47:53 +0300

    add mysql dep

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mysql = require("mysql2");
 const app = express();
+
+const connection = mysql.createConnection({
+    host: "ruvds-ueoe3",
+    user: "root",
+    database: "test",
+    password: "Password123#@!"
+});
+
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+});
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
